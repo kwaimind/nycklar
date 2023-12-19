9c94cb5bd2e7ba00aa26f776f384a382f026e84c
Daniel Reed <danieel.reed@gmail.com>
2023-12-19 14:42:42 +0100

    refactor: adding types and removing debounce dependency

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,79 @@
-import debounce from "debounce";
-
 type TEventsMap = Readonly<Record<string, (event: KeyboardEvent) => void>>;
 
+const DEFAULT_TIMEOUT = 1000;
+
 const DEFAULT_KEY = "keydown";
+
+const eventType = [DEFAULT_KEY, "keyup"] as const;
+
+type TEventType = (typeof eventType)[number];
+
+interface TOptions {
+  timeout?: number;
+  event?: TEventType;
+}
 
 /**
  * A basic prototype of the tinykeys lib
  * Takes a target and a map of key events to listen to an a function to call when matched
+ * Returns an unsubscribe method.
  * @see https://github.com/jamiebuilds/tinykeys
+ * @example
+ * ```js
+ * import { nycklar } from "../src/nycklar"
+ *
+ * nycklar(window, {
+ * 	"d": () => {
+ * 		console.log("pressed d");
+ * 	},
+ * 	"abc": () => {
+ * 		console.log("pressed abc");
+ * 	},
+ * 	"Shift+D": () => {
+ * 		console.log("pressed shift d");
+ * 	},
+ * })
+ * ```
  */
-const nycklar = (target: Window | HTMLElement, keyEvents: TEventsMap) => {
+export const nycklar = (
+  target: Window | HTMLElement,
+  keyEvents: TEventsMap,
+  options: TOptions = {}
+) => {
   let keysPressed: string[] = [];
 
-  const assertKeys = debounce((key: string, event: KeyboardEvent) => {
+  const { timeout = DEFAULT_TIMEOUT, event = DEFAULT_KEY } = options;
+
+  let timer: number | null = null;
+
+  const assertKeys = (key: string, event: KeyboardEvent) => {
     if (key in keyEvents) {
       keyEvents[key](event);
     }
     keysPressed = [];
-  }, 250);
+  };
 
-  const mapEvents = (e: KeyboardEvent) => {
+  const mapEvents = (e: Event) => {
+    if (!(e instanceof KeyboardEvent)) return;
     const hasMetaKey = e.altKey || e.ctrlKey || e.shiftKey;
     keysPressed.push(e.key);
     const result = keysPressed.join(hasMetaKey ? "+" : "");
-    assertKeys(result, e);
+    if (typeof timer === "number") {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      assertKeys(result, e);
+    }, timeout);
   };
 
-  const createListener = (e: Event) => {
-    if (!(e instanceof KeyboardEvent)) return;
-    mapEvents(e);
-  };
-
-  target.addEventListener(DEFAULT_KEY, createListener);
+  target.addEventListener(event, mapEvents);
 
   return () => {
-    target.removeEventListener(DEFAULT_KEY, createListener);
+    target.removeEventListener(event, mapEvents);
   };
 };
 
-const events = {
+nycklar(window, {
   d: () => {
     console.log("pressed d");
   },
@@ -48,6 +83,4 @@ const events = {
   "Shift+D": () => {
     console.log("pressed shift d");
   },
-};
-
-nycklar(window, events);
+});
