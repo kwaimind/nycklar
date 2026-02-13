0ef02e5a0892fca369930ef8c5b469d5d3567d2a
Daniel Reed <danieel.reed@gmail.com>
2026-02-13 15:15:55 +0100

    refactor: updating build tools

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,88 +1,89 @@
+import { afterEach, describe, expect, it, vi } from "vitest";
 import { nycklar } from ".";
 
-jest.useFakeTimers();
+vi.useFakeTimers();
 
 describe("nycklar", () => {
-  const keyEvents = {
-    a: jest.fn(),
-    ab: jest.fn(),
-    "Shift+D": jest.fn(),
-  };
+	const keyEvents = {
+		a: vi.fn(),
+		ab: vi.fn(),
+		"Shift+D": vi.fn(),
+	};
 
-  afterEach(() => {
-    jest.clearAllTimers();
-    jest.resetAllMocks();
-  });
+	afterEach(() => {
+		vi.clearAllTimers();
+		vi.resetAllMocks();
+	});
 
-  const createKeyboardEvent = (
-    key: string,
-    options: KeyboardEventInit = {}
-  ): KeyboardEvent => new KeyboardEvent("keydown", { key, ...options });
+	const createKeyboardEvent = (
+		key: string,
+		options: KeyboardEventInit = {},
+	): KeyboardEvent => new KeyboardEvent("keydown", { key, ...options });
 
-  it("should handle single key press", async () => {
-    nycklar(window, keyEvents);
+	it("should handle single key press", async () => {
+		nycklar(window, keyEvents);
 
-    const keydownEvent = createKeyboardEvent("a");
-    window.dispatchEvent(keydownEvent);
+		const keydownEvent = createKeyboardEvent("a");
+		window.dispatchEvent(keydownEvent);
 
-    jest.runAllTimers();
+		vi.runAllTimers();
 
-    expect(keyEvents.a).toHaveBeenCalled();
-  });
+		expect(keyEvents.a).toHaveBeenCalled();
+	});
 
-  it("should return an unsubscribe function", async () => {
-    const cleanup = nycklar(window, keyEvents);
-    expect(cleanup).toEqual(expect.any(Function));
-  });
+	it("should return an unsubscribe function", async () => {
+		const cleanup = nycklar(window, keyEvents);
+		expect(cleanup).toEqual(expect.any(Function));
+	});
 
-  it("should handle multiple key presses", () => {
-    nycklar(window, keyEvents);
+	it("should handle multiple key presses", () => {
+		nycklar(window, keyEvents);
 
-    const eventA = createKeyboardEvent("a");
-    const eventB = createKeyboardEvent("b");
-    window.dispatchEvent(eventA);
-    window.dispatchEvent(eventB);
+		const eventA = createKeyboardEvent("a");
+		const eventB = createKeyboardEvent("b");
+		window.dispatchEvent(eventA);
+		window.dispatchEvent(eventB);
 
-    jest.runOnlyPendingTimers();
+		vi.runOnlyPendingTimers();
 
-    expect(keyEvents.ab).toHaveBeenCalled();
-    expect(keyEvents.a).not.toHaveBeenCalled();
-  });
+		expect(keyEvents.ab).toHaveBeenCalled();
+		expect(keyEvents.a).not.toHaveBeenCalled();
+	});
 
-  it("should handle shift key presses", () => {
-    nycklar(window, keyEvents);
+	it("should handle shift key presses", () => {
+		nycklar(window, keyEvents);
 
-    const eventA = createKeyboardEvent("Shift", { shiftKey: true });
-    const eventB = createKeyboardEvent("D", { shiftKey: true });
-    window.dispatchEvent(eventA);
-    window.dispatchEvent(eventB);
+		const eventA = createKeyboardEvent("Shift", { shiftKey: true });
+		const eventB = createKeyboardEvent("D", { shiftKey: true });
+		window.dispatchEvent(eventA);
+		window.dispatchEvent(eventB);
 
-    jest.runOnlyPendingTimers();
+		vi.runOnlyPendingTimers();
 
-    expect(keyEvents["Shift+D"]).toHaveBeenCalled();
-    expect(keyEvents.ab).not.toHaveBeenCalled();
-  });
+		expect(keyEvents["Shift+D"]).toHaveBeenCalled();
+		expect(keyEvents.ab).not.toHaveBeenCalled();
+	});
 
-  it("should ignore non-keyboard events", () => {
-    nycklar(window, keyEvents);
+	it("should ignore non-keyboard events", () => {
+		nycklar(window, keyEvents);
 
-    const eventA = new MouseEvent("click");
-    window.dispatchEvent(eventA);
+		const eventA = new MouseEvent("click");
+		window.dispatchEvent(eventA);
 
-    jest.runOnlyPendingTimers();
+		vi.runOnlyPendingTimers();
 
-    expect(keyEvents.a).not.toHaveBeenCalled();
-  });
+		expect(keyEvents.a).not.toHaveBeenCalled();
+	});
 
-  it("should ignore input events", () => {
-    const target = document.createElement("input");
-    nycklar(target, keyEvents);
+	it("should ignore input events", () => {
+		const target = document.createElement("input");
+		nycklar(target, keyEvents);
 
-    const eventA = createKeyboardEvent("a");
-    target.dispatchEvent(eventA);
+		const eventA = createKeyboardEvent("a");
+		target.dispatchEvent(eventA);
 
-    jest.runOnlyPendingTimers();
+		vi.runOnlyPendingTimers();
 
-    expect(keyEvents.a).not.toHaveBeenCalled();
-  });
+		expect(keyEvents.a).not.toHaveBeenCalled();
+	});
 });
